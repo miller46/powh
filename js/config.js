d33ab0b72595aa3a3727f5fab7bacb12d95a1745
Jack Miller <jack@edziedzic-win7.dcmllc.local>
2018-01-30 14:29:34 -0600

    publish

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var config = {
     "isTestnet": false,
-    "siteUrl": "http://localhost:8000",
+    "siteUrl": "https://miller46.github.io/powh/",
     "contractFileNameBase": "../contract/powh",
     "contractAddress": "0xA7CA36F7273D4d38fc2aEC5A454C497F86728a7A",
     "etherscanApiKey": "JEEFXW352DSRW9JX9J549YQ91I5Y29SNHK",
