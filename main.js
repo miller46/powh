e5e16eccf41c57a5c4a1d97cb79119041c01bfc1
Jack Miller <jack@Jacks-MacBook-Pro.local>
2018-01-31 00:04:38 -0600

    add shares worth

--- main.js
+++ main.js
@@ -7,11 +7,21 @@ var powhContract;
 var usdPrice;
 var tokenPrice;
 
-var userCookie = cookieUtility.readCookie(config.userCookie);
-if (userCookie) {
-    userCookie = JSON.parse(userCookie);
-    var address = userCookie["address"];
+var address = getSavedAddress();
+if (address) {
     $('#balance_address').val(address);
+} else if (web3.eth.defaultAccount) {
+    $('#balance_address').val(web3.eth.defaultAccount);
+}
+
+function getSavedAddress() {
+    var userCookie = cookieUtility.readCookie(config.userCookie);
+    if (userCookie) {
+        userCookie = JSON.parse(userCookie);
+        return userCookie["address"];
+    }
+
+    return false;
 }
 
 web3Utility.loadContract(web3, config.contractFileNameBase, config.contractAddress, function(err, contract) {
@@ -20,53 +30,77 @@ web3Utility.loadContract(web3, config.contractFileNameBase, config.contractAddre
     } else {
         powhContract = contract;
 
-        var userCookie = cookieUtility.readCookie(config.userCookie);
-        if (userCookie) {
-            userCookie = JSON.parse(userCookie);
-            var address = userCookie["address"];
-            lookupAddress(address);
-        }
-
         loadData();
 
         setInterval(loadData, 3000);
 
-        web3.eth.filter("latest").watch(function(err, result) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(result);
-                web3.eth.getBlock(result, true, function(err, result) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log(result);
-                        var txs = result.transactions;
-                        for (var i = 0; i < txs.length; i++) {
-                            var tx = txs[i];
-                            if (tx.to) {
-                                if (tx.to.toLowerCase() === config.contractAddress.toLowerCase()) {
-                                    var txLink = "<a target='_blank' href='https://etherscan.io/tx/" + tx.hash + "'>View</a>";
-                                    var userLink = "<a target='_blank' href='https://etherscan.io/address/" + tx.from + "'>" + tx.from + "</a>";
-                                    var input = tx.input;
-                                    var action = "<td>Internal Tx</td>";
-                                    if (tx.value.toNumber() > 0) {
-                                        action = "<td class='profit'>BUY</td>";
-                                    } else {
-                                        action = "<td class='loss'>WEAK HANDS</td>";
-                                    }
-                                    var amount = web3Utility.weiToEth(tx.value.toNumber(), undefined, 4);
-
-                                    $('#tx_table tr:last').after('<tr><td>' + txLink + '</td><td>' + userLink + '</td>' + action + '<td>' + amount + ' ETH</td></tr>');
-                                }
-                            }
-                        }
-                    }
-                });
-            }
-        });
+        watchForLatestTransactions();
     }
 });
+
+
+function watchForLatestTransactions() {
+    web3.eth.getBlockNumber(function(err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            var blockNumber = result;
+            var options = {
+                fromBlock: blockNumber - 25,
+                address: config.contractAddress,
+            };
+            var filter = web3.eth.filter(options);
+            filter.watch(function(err, result) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(result);
+                    var blockHash = result.blockHash;
+                    web3.eth.getBlock(blockHash, true, function(err, result) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log(result);
+                            var txs = result.transactions;
+                            for (var i = 0; i < txs.length; i++) {
+                                var tx = txs[i];
+                                tx.timestamp = result.timestamp;
+                                addTxToTableIfNecessary(tx);
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
+
+function addTxToTableIfNecessary(tx) {
+    if (tx.to && tx.to.toLowerCase() === config.contractAddress.toLowerCase()) {
+        var now = Math.round(new Date().getTime() / 1000);
+        var timestamp = tx.timestamp;
+        var timeAgo = "";
+        var diff = now - timestamp;
+        if (diff < 60) {
+            timeAgo = diff.toFixed(0) + " seconds ago";
+        } else if (diff < 3600) {
+            timeAgo = (diff / 60).toFixed(0) + " minutes ago";
+        } else {
+            timeAgo = (diff / 3600).toFixed(0) + " hours ago";
+        }
+        var userLink = "<a target='_blank' href='https://etherscan.io/address/" + tx.from + "'>" + tx.from + "</a>";
+        var input = tx.input;
+        var action = "<td>Internal Tx</td>";
+        if (tx.value.toNumber() > 0) {
+            action = "<td class='profit'>BUY</td>";
+        } else {
+            action = "<td class='loss'>WEAK HANDS</td>";
+        }
+        var amount = web3Utility.weiToEth(tx.value.toNumber(), undefined, 4);
+
+        $('#tx_table tbody tr:first').before('<tr><td>' + timeAgo + '</td><td>' + userLink + '</td>' + action + '<td>' + amount + ' ETH</td></tr>');
+    }
+}
 
 function loadData() {
     web3Utility.getPriceUsd(function(err, price) {
@@ -75,6 +109,13 @@ function loadData() {
         } else {
             usdPrice = price;
             $('.usdPrice').show();
+
+            var address = getSavedAddress();
+            if (address) {
+                lookupAddress(address);
+            } else if (web3.eth.defaultAccount) {
+                lookupAddress(web3.eth.defaultAccount);
+            }
 
             web3Utility.call(web3, powhContract, config.contractAddress, 'buyPrice', {}, function (err, result) {
                 if (err) {
@@ -141,9 +182,23 @@ function lookupAddress(address) {
         if (err) {
             console.log(err);
         } else {
-            var balance = web3Utility.weiToEth(result.toNumber(), 1000000000000000, 4);
-            $('#balanceEth').text(balance);
+            var balance = result.toNumber() / Math.pow(10, 15);
+            $('#balanceEth').text(balance.toFixed(4));
             $('#balanceUsd').text("$" + toDollars(balance * tokenPrice * usdPrice));
+
+            web3Utility.call(web3, powhContract, config.contractAddress, 'sellPrice', {}, function (err, result) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    var sellPrice = result.toNumber() / Math.pow(10, 18);
+                    var shareWorth = balance * sellPrice;
+
+                    setClassNameForPriceField("shareWorth", shareWorth);
+
+                    $('#shareWorth').text(shareWorth.toFixed(6));
+                    $('#shareWorthUsd').text("$" + toDollars(shareWorth * usdPrice));
+                }
+            });
 
             $('#values').show();
         }
@@ -153,7 +208,7 @@ function lookupAddress(address) {
         if (err) {
             console.log(err);
         } else {
-            var dividends = web3Utility.weiToEth(result.toNumber());
+            var dividends = web3Utility.weiToEth(result.toNumber(), undefined, 6);
             $('#dividendEth').text(dividends);
             $('#dividendUsd').text("$" + toDollars(dividends * usdPrice));
 
